c7beafe75df38e858bd57846efc5d72e4735d402
robotsarecrazy64 <jkr232@nau.edu>
2019-10-16 12:15:02 -0700

    Cleaned up code

--- game.js
+++ game.js
@@ -27,6 +27,7 @@ var goal;
 var big_skull_a;
 var big_skull_b;
 var big_skull_c;
+var game_active = false;
 
 // Variables to improve readability
 var ground_level = 423;
@@ -80,6 +81,7 @@ function generateLevel() {
 	game_stage.addChild( player );
 	
 	// Set up enemies
+<<<<<<< Updated upstream
 	big_skull_a = createSprite( 0, 0, 1, 1, "big_flaming_skull.png" );
 	big_skull_a.anchor.x = 0.5;
 	big_skull_a.anchor.y = 0.5;
@@ -114,10 +116,35 @@ function generateLevel() {
 	game_stage.addChild( skull_c );
 	
 	master_stage.addChild( game_stage );
+=======
+        big_skull_a = createSkull( skull_a, end_of_map/4 );
+	big_skull_b = createSkull( skull_b, end_of_map/2 );
+	big_skull_c = createSkull ( skull_c, ( 3 * end_of_map ) / 4 );
+	
+        master_stage.addChild( game_stage );
+	buildScreens();
+>>>>>>> Stashed changes
 	
 	update();
 }
 
+/**
+	Creates the enemy sprite
+*/
+function createSkull( stage, position ) {
+	var big_skull = createSprite( 0, 0, 1, 1, "big_flaming_skull.png" );
+	big_skull.anchor.x = 0.5;
+	big_skull.anchor.y = 0.5;
+	big_skull.interactive = true;
+	stage.position.x = position;
+	stage.position.y = ground_level - tile_size;
+	stage.pivot.x = 50;
+	stage.pivot.y = 50;
+    stage.rotation = 0;
+	stage.addChild( big_skull );
+	game_stage.addChild( stage );
+	return big_skull;
+}
 /**
 	Builds the Floor Tiles Recursively
 */
@@ -151,6 +178,14 @@ function update() {
 	// Update renderer
 	renderer.render( master_stage );
 	requestAnimationFrame( update );
+<<<<<<< Updated upstream
+=======
+   
+   if( game_active )
+   { document.addEventListener( 'keydown', keydownEventHandler ); }
+   else 
+   { document.removeEventListener( 'keydown', keydownEventHandler ); }
+>>>>>>> Stashed changes
 }
 
 /**
@@ -202,6 +237,7 @@ function addEnemy( image ) {
 */
 function checkWinCondition () {
 	if( player.x >= goal.x ) {
+<<<<<<< Updated upstream
 		
 		if(( count % 2 ) == 0 ) { // prevents excessive alerts
 			winner = true;
@@ -211,6 +247,11 @@ function checkWinCondition () {
 		
 			
 		count += 1;
+=======
+		winner = true;
+		endScreen.visible = true;
+		game_active = false;
+>>>>>>> Stashed changes
 	}
 }
 
@@ -234,6 +275,135 @@ function createSprite (x, y, scale_x, scale_y, image ) {
 	sprite.scale.y = scale_x;
 	return sprite;
 }
+/**
+ var graphics = new PIXI.Graphics();
+   graphics.beginFill( BLACK );
+   graphics.drawRect(0, 0, 1000, 500);
+   graphics.endFill();
+   startScreen.addChild( graphics );
+*/
+function createShape() {
+   var graphics = new PIXI.Graphics();
+   graphics.beginFill(0x000000);
+   graphics.drawRect(0, 0, 1000, 500);
+   graphics.endFill();
+   return graphics;
+}
+
+function buildScreens() {
+   instructScreen.visible = false;
+   creditScreen.visible = false;
+   //difficultyScreen.visible = false;
+   endScreen.visible = false;
+
+    // Text for titles
+   var gameTitleText = new PIXI.Text( "Cave Escape!", titleStyle );
+   var gameInstructTitleText = new PIXI.Text( "Instructions", titleStyle );
+   var gameCreditTitleText = new PIXI.Text( "Credits", titleStyle );
+   var gameEndText = new PIXI.Text( "Game over!\nYou win!", endStyle );
+
+   // Text for title screen options
+   var gameStartText = new PIXI.Text( "Start", selectionStyle );
+   var gameInstructText = new PIXI.Text( "Instructions", selectionStyle );
+   var gameCredText = new PIXI.Text( "Credits", selectionStyle );
+   var gameCredBackText = new PIXI.Text( "<- Back", backStyle );
+   var gameInstructBackText = new PIXI.Text( "<- Back", backStyle );
+   var gameRestartText = new PIXI.Text( "Play again", selectionStyle );
+   var gameReturnTitleText = new PIXI.Text( "Back to title screen", selectionStyle );
+   
+   // Adds regular text
+   var gameInstructDesc = new PIXI.Text( "The goal of the game is to navigate the cave and" + 
+      " make it\nto the end! The character is moved using the A and D keys.\n\nMove the character" + 
+      " to the end of the cave to win.", selectionStyle );
+   var gameCredDesc = new PIXI.Text( "Authors: John Jacobelli\nJesse Rodriguez\nTyler Pehringer\n\nRenderer used: PixiJS", 
+      selectionStyle );
+
+   // Declare texts interactable
+   gameStartText.interactive = true;
+   gameInstructText.interactive = true;
+   gameCredText.interactive = true;
+   gameCredBackText.interactive = true;
+   gameInstructBackText.interactive = true;
+   gameRestartText.interactive = true;
+   gameReturnTitleText.interactive = true;
+   
+   // Declares interactable text functions
+   gameStartText.click = function(event) { startScreen.visible = false;
+					   game_active = true; }
+   gameInstructText.click = function(event) { instructScreen.visible = true;
+                                              startScreen.visible = false; }
+   gameCredText.click = function(event) { creditScreen.visible = true;
+                                          startScreen.visible = false; }
+   gameCredBackText.click = function(event) { startScreen.visible = true;
+                                              creditScreen.visible = false; }
+   gameInstructBackText.click = function(event) { startScreen.visible = true;
+                                                  instructScreen.visible = false; }
+   gameRestartText.click = function(event) { endScreen.visible = false; 
+                                             player.position.x = 0;
+					     game_active = true; 
+                                             winner = false; 
+                                             clearStage();
+                                             generateLevel(); }
+   gameReturnTitleText.click = function(event) { startScreen.visible = true;
+                                                 endScreen.visible = false; 
+                                                 player.position.x = 0; 
+                                                 winner = false; }
+   
+   // Create background for screens screen
+   var graphics = createShape();
+   startScreen.addChild( graphics );
+   instructScreen.addChild( graphics );
+   creditScreen.addChild( graphics );
+   endScreen.addChild( graphics );
+
+   // Add text to screens
+   startScreen.addChild( gameTitleText );
+   startScreen.addChild( gameStartText );
+   startScreen.addChild( gameInstructText );
+   startScreen.addChild( gameCredText );
+   instructScreen.addChild( gameInstructTitleText );
+   instructScreen.addChild( gameInstructDesc );
+   instructScreen.addChild( gameInstructBackText );
+   creditScreen.addChild( gameCredBackText );
+   creditScreen.addChild( gameCreditTitleText );
+   creditScreen.addChild( gameCredDesc );
+   endScreen.addChild( gameEndText );
+   endScreen.addChild( gameRestartText );
+   endScreen.addChild( gameReturnTitleText );
+   
+   // Set anchors for text
+   gameTitleText.anchor.set( .5 );
+   gameStartText.anchor.set( .5 );
+   gameInstructText.anchor.set( .5 );
+   gameCredText.anchor.set( .5 );
+   gameInstructTitleText.anchor.set( .5 );
+   gameInstructBackText.anchor.set( 1 );
+   gameCredBackText.anchor.set( 1 );
+   gameCreditTitleText.anchor.set( .5 );
+   gameEndText.anchor.set( .5 );
+   gameRestartText.anchor.set( .5 );
+   gameReturnTitleText.anchor.set( .5 );
+
+   // Place Text
+   gameTitleText.x = renderer.width/2; gameTitleText.y = renderer.height/4;
+   gameStartText.x = renderer.width/6; gameStartText.y = renderer.height/4 * 3;
+   gameInstructText.x = renderer.width/2 ; gameInstructText.y = renderer.height/4 * 3;
+   gameCredText.x = renderer.width/6 * 5; gameCredText.y = renderer.height/4 * 3;
+   gameInstructTitleText.x = renderer.width/2; gameInstructTitleText.y = renderer.height/4;
+   gameInstructDesc.x = 25; gameInstructDesc.y = renderer.height/2;
+   gameCreditTitleText.x = renderer.width/2; gameCreditTitleText.y = renderer.height/4;
+   gameCredDesc.x = 25; gameCredDesc.y = renderer.height/2;
+   gameInstructBackText.x = 975; gameInstructBackText.y = 475;
+   gameCredBackText.x = 975; gameCredBackText.y = 475;
+   gameEndText.x = renderer.width/2; gameEndText.y = renderer.height/3 + 10;
+   gameRestartText.x = renderer.width/2; gameRestartText.y = renderer.height/2 + 50;
+   gameReturnTitleText.x = renderer.width/2; gameReturnTitleText.y = renderer.height/2 + 100;
+   
+   master_stage.addChild( startScreen );
+   master_stage.addChild( instructScreen );
+   master_stage.addChild( creditScreen );
+   master_stage.addChild( endScreen );
+}
 
 /**
 	Helper function that creates a tile
@@ -246,6 +416,23 @@ function createTile (x, y, size, sprite ) {
 	return tile;
 }
 
+/**
+	Helper function that clears the stage
+*/
 function clearStage () {
+<<<<<<< Updated upstream
 	master_stage.removeChild( game_stage );
+=======
+   for (var i = skull_a.children.length - 1; i >= 0; i--) { skull_a.removeChild(skull_a.children[i]);};
+   for (var i = skull_b.children.length - 1; i >= 0; i--) { skull_b.removeChild(skull_b.children[i]);};
+   for (var i = skull_c.children.length - 1; i >= 0; i--) { skull_c.removeChild(skull_c.children[i]);};
+   for (var i = startScreen.children.length - 1; i >= 0; i--) { startScreen.removeChild(startScreen.children[i]);};
+   for (var i = instructScreen.children.length - 1; i >= 0; i--) { instructScreen.removeChild(instructScreen.children[i]);};
+   for (var i = creditScreen.children.length - 1; i >= 0; i--) { creditScreen.removeChild(creditScreen.children[i]);};
+   for (var i = endScreen.children.length - 1; i >= 0; i--) { endScreen.removeChild(endScreen.children[i]);};
+   for (var i = back.children.length - 1; i >= 0; i--) { back.removeChild(back.children[i]);};
+   for (var i = game_stage.children.length - 1; i >= 0; i--) { game_stage.removeChild(game_stage.children[i]);};
+   for (var i = master_stage.children.length - 1; i >= 0; i--) { master_stage.removeChild(master_stage.children[i]);};
+   delete master_stage;
+>>>>>>> Stashed changes
 }
